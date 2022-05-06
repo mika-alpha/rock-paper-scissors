758f439e25d4332e35e149e85e43b21468fe2f38
mika-alpha <yuukeikioku@gmail.com>
2022-05-06 16:21:41 -0500

    Add playRound function

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 let computerPlay = () => {
-    let options = ["Rock", "Paper", "Scissors"];
+    let options = ["rock", "paper", "scissors"];
     let pick = Math.floor(Math.random()*3);
     return options[pick];
 };
+
+function playRound(playerSelection, computerSelection){
+
+    if (playerSelection.toLowerCase() === computerSelection.toLowerCase()){
+        return "It's a draw!";
+    } else if (playerSelection.toLowerCase() === "paper"){
+        if (computerSelection === "scissors"){
+            return "You lose! Scissors beats Paper";
+        }
+    } else if (playerSelection.toLowerCase() === "scissors"){
+        if (computerSelection === "rock"){
+            return "You lose! Rock beats Scissors";
+        }
+    } else if (playerSelection.toLowerCase() === "rock"){
+        if (computerSelection === "paper"){
+            return "You lose! Paper beats Rock";
+        }
+    } 
+    return `You won! ${playerSelection} beats ${computerSelection}`;
+}
+
+
 
 
