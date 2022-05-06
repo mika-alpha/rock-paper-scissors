a5f75cc8525615d831115e9bc2da8994dc466105
mika-alpha <yuukeikioku@gmail.com>
2022-05-06 16:40:04 -0500

    Add game function

--- script.js
+++ script.js
@@ -7,23 +7,50 @@ let computerPlay = () => {
 function playRound(playerSelection, computerSelection){
 
     if (playerSelection.toLowerCase() === computerSelection.toLowerCase()){
-        return "It's a draw!";
+        console.log("It's a draw!");
+        return 0;
     } else if (playerSelection.toLowerCase() === "paper"){
         if (computerSelection === "scissors"){
-            return "You lose! Scissors beats Paper";
+            console.log("You lose! Scissors beats Paper");
+            return -1;
         }
     } else if (playerSelection.toLowerCase() === "scissors"){
         if (computerSelection === "rock"){
-            return "You lose! Rock beats Scissors";
+            console.log("You lose! Rock beats Scissors");
+            return -1;
         }
     } else if (playerSelection.toLowerCase() === "rock"){
         if (computerSelection === "paper"){
-            return "You lose! Paper beats Rock";
+            console.log("You lose! Paper beats Rock");
+            return -1;
         }
     } 
-    return `You won! ${playerSelection} beats ${computerSelection}`;
+    console.log(`You won! ${playerSelection} beats ${computerSelection}`);
+    return 1;
 }
 
+function game(){
+    let playerScore = 0;
+    let computerScore = 0;
 
+    for (let i = 0; i < 5; i++){
+        let result = playRound(prompt("Rock, Papper or Scissors: "), computerPlay());
+        if (result === 1){
+            playerScore++;
+        } else if (result === -1){
+            computerScore++;
+        }
+    }
+    
+    if (playerScore > computerScore){
+        return "The player has won!!"   
+    } else if (playerScore < computerScore){
+        return "The computer has won!!"
+    } else {
+        return "It's a draw!! no one has won!!"
+    }
+}
+
+console.log(game());
 
 
